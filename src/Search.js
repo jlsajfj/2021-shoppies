647271ba12cf32c9983d6e36b4fd4eeddb3c375d
jlsajfj <jlsajfj@hotmail.com>
2021-05-06 23:10:50 -0400

    added basic searchbar functionality to log to console

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,38 @@
 import search_icon from './search_icon.svg';
 import './Search.css';
+import React from 'react';
 
-function Search() {
-    function abc(){
+class Search extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  return (
-    <div className="search-body">
-        <div className="search-title">Search for Movies!</div>
-        <div className="search-input">
-            <img src={search_icon} alt="Magnifying glass" className="search-icon"/>
-            <input onChange={abc}
-                className="search-text"
-                placeholder="Type here to search"/>
-        </div>
-    </div>
-  );
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        console.log(this.state.value);
+        event.preventDefault();
+    }
+
+    render () {
+        return (
+            <div className="search-body">
+                <div className="search-title">Search for Movies!</div>
+                <form className="search-input" onSubmit={this.handleSubmit}>
+                    <img src={search_icon} alt="Magnifying glass" className="search-icon"/>
+                    <input onChange={this.handleChange}
+                        className="search-text"
+                        placeholder="Type here to search"/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Search;
