7fe4e56a6cf3427b462a918dc75e6930125b7ee8
jlsajfj <jlsajfj@hotmail.com>
2021-05-06 22:59:55 -0400

    starting search with seachbar

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,19 @@
+import search_icon from './search_icon.svg';
 import './Search.css';
 
 function Search() {
+    function abc(){
+
+    }
   return (
-    <div>
-        search goes here
+    <div className="search-body">
+        <div className="search-title">Search for Movies!</div>
+        <div className="search-input">
+            <img src={search_icon} alt="Magnifying glass" className="search-icon"/>
+            <input onChange={abc}
+                className="search-text"
+                placeholder="Type here to search"/>
+        </div>
     </div>
   );
 }
