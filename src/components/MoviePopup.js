faec8471eb62e7351679e515706bf0fd0717f1b2
jlsajfj <jlsajfj@hotmail.com>
2021-05-07 23:03:43 -0400

    added "click outside to close" functionality

--- src/components/MoviePopup.js
+++ src/components/MoviePopup.js
@@ -4,14 +4,29 @@ class MoviePopup extends React.Component {
     constructor(props){
         super(props);
         this.state = { movieData: props.data }
+        this.wrapperRef = React.createRef();
+        this.outerClick = this.outerClick.bind(this)
     }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.outerClick);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.outerClick);
+    }
+
+    outerClick(event){
+        if(this.wrapperRef && !this.wrapperRef.current.contains(event.target)){
+            this.props.hide()
+        }
+    }
+
     render(){
         return (
         <div className="fade-in">
-            <div className="movie-overlay" onClick={ this.props.hide }>
-                <div className="movie-display border">
-                    abc
-                </div>
+            <div className="movie-display border" ref={this.wrapperRef}>
+                abc
             </div>
         </div>
         );
